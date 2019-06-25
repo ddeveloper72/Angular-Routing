5be386cc456a3f40c101d3f8dfbbbfdca0cb0c7d
ddeveloper72 <duncan.niamh@gmail.com>
2019-06-25 22:36:01 +0100

    Updated ngx bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { ServersService } from './servers/servers.service';
   imports: [
     BrowserModule,
     FormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
