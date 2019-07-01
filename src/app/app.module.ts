dd0d6f38d9e6cc55999a7037429f9ec725044302
ddeveloper72 <duncan.niamh@gmail.com>
2019-07-01 22:54:18 +0100

    Refactored app routing to a new app-routing module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,21 +12,9 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 
-const appRoutes: Routes = [
-{ path: '', component: HomeComponent },
-{ path: 'users', component: UsersComponent, children: [
-  { path: ':id/:name', component: UserComponent },
-]},
-{ path: 'servers', component: ServersComponent, children: [
-  { path: ':id', component: ServerComponent },
-  { path: ':id/edit', component: EditServerComponent },
-] },
-{ path: 'not-found', component: PageNotFoundComponent},
-{ path: '**', redirectTo: '/not-found', pathMatch: 'full'}
-];
 @NgModule({
   declarations: [
     AppComponent,
@@ -42,7 +30,7 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
